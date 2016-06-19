594d951950425fd3c22bb35f42694c9d58dc27cb
Doug Turnbull <softwaredoug@gmail.com>
2016-06-19 14:56:41 -0400

    Fetches/displays one poster path

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,8 +1,28 @@
-angular.module('recsApp', []);
+angular.module('recsApp', ['elasticsearch']);
 
-angular.module('recsApp', [])
-  .controller('LikedMoviesCtrl', function($scope) {
+
+angular.module('recsApp')
+.service('esClient', function (esFactory) {
+  return esFactory({
+    host: 'localhost:9200',
+  });
+});
+
+
+angular.module('recsApp')
+  .controller('LikedMoviesCtrl', function($scope, esClient) {
     var movies = this;
+
+    esClient.get({
+      index: 'ml_tmdb',
+      type: 'movie',
+      id: 1
+    }).then(function(response) {
+      var movie = response._source;
+      movies.movies[0] = movie;
+      console.log(response);
+    });
+
     movies.movies = [
       {'title': 'fooshank redemption'},
       {'title': 'barest gump'}
