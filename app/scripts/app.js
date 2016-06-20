10fc548ccf85784f8cfebe5085778c714ef0be13
Doug Turnbull <softwaredoug@gmail.com>
2016-06-19 22:03:20 -0400

    Fetches movies a user likes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,6 +8,45 @@ angular.module('recsApp')
   });
 });
 
+angular.module('recsApp')
+  .controller('RecsUserCtrl', function(esClient) {
+    var recUsers = this;
+    recUsers.users = {
+      '123': {name: 'Tom',  id: '123',  likedMovies: null},
+      '5251': {name: 'Sue', id: '5251', likedMovies: null},
+      '528': {name: 'Ed', id: '528', likedMovies: null},
+      '17': {name: 'Kate', id: '17', likedMovies: null}
+    };
+
+    recUsers.refetchProfile = function() {
+      // fetch this users baskets
+      esClient.get({
+        index: 'movielens',
+        type: 'user',
+        id: recUsers.selected}).then(function(resp) {
+          var likedMovies = resp._source.liked_movies;
+          recUsers.currUser = recUsers.users[recUsers.selected];
+
+          esClient.mget({
+            index: 'ml_tmdb',
+            type: 'movie',
+            body: {ids: likedMovies}}).then(function(resp) {
+              var movies = [];
+              angular.forEach(resp.docs, function(doc) {
+                var movie = doc._source;
+                movies.push(movie);
+              });
+              recUsers.currUser.likedMovies = movies;
+              console.log(resp);
+            });
+
+        });
+    };
+    recUsers.currUser = null;
+    recUsers.selected = null;
+
+  });
+
 
 angular.module('recsApp')
   .controller('LikedMoviesCtrl', function($scope, esClient) {
