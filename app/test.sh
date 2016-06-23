dd3442093d541fcd877527463a4f1fd3494757ba
Doug Turnbull <softwaredoug@gmail.com>
2016-06-23 10:11:12 -0400

    Sets log level to debug

--- app/test.sh
+++ app/test.sh
@@ -2,4 +2,4 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
-node_modules/karma/bin/karma start  --single-run
+node_modules/karma/bin/karma start --log-level debug --single-run
