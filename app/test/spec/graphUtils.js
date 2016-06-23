e58a2c97d9282091a9f41f30f77187bbdd02f2cd
Doug Turnbull <softwaredoug@gmail.com>
2016-06-23 12:27:42 -0400

    Fixes inject call in script
    Note this form doesn't swallow exceptions on module load errors

--- app/test/spec/graphUtils.js
+++ app/test/spec/graphUtils.js
@@ -2,10 +2,12 @@ describe('Graph Utilities', function() {
   beforeEach(module('recsApp'));
 
   var graphUtils = null;
-  beforeEach(inject(function(_graphUtils_) {
-    console.log('Injecting ' + _graphUtils_);
-    graphUtils = _graphUtils_;
-  }));
+
+  beforeEach(function() {
+    inject(function(_graphUtils_) {
+      graphUtils = _graphUtils_;
+    });
+  });
 
   it('has a property', function() {
     console.log('Running test ');
