3d4e22fcaa38a3926de16586c06071de2e71dfb4
Daniel Beach <dbeach@opensourceconnections.com>
2016-09-23 13:09:34 -0400

    Show example user profile on as default

--- app/scripts/appModeCtrl.js
+++ app/scripts/appModeCtrl.js
@@ -1,6 +1,5 @@
 angular.module('recsApp')
   .controller('AppModeCtrl', function(esClient, recsSvc) {
     var appMode = this;
-    appMode.tabSelect = 'custom';
-
+    appMode.tabSelect = 'profile';
   });
