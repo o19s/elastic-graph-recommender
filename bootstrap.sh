05d3b5544b5b27b60678d77320d8562f163ed5e5
Doug Turnbull <softwaredoug@gmail.com>
2016-09-23 10:16:54 -0400

    Add script for provisioning Elasticsearch instance with graph

---
+++ bootstrap.sh
@@ -0,0 +1,14 @@
+sudo add-apt-repository ppa:openjdk-r/ppa  
+sudo apt-get update   
+sudo apt-get install openjdk-7-jdk  
+wget https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/deb/elasticsearch/2.4.0/elasticsearch-2.4.0.deb
+sudo dpkg -i elasticsearch-2.4.0.deb
+sudo update-rc.d elasticsearch enable
+
+sudo echo "network.host: 0.0.0.0" >>  /etc/elasticsearch/elacticsearch.yml
+sudo echo "http.cors.allow-origin: "/.*/"" >> /etc/elasticsearch/elasticsearch.yml
+sudo echo "http.cors.enabled: true" >> /etc/elasticsearch/elacticsearch.yml
+
+cd /usr/share/elasticsearch/
+bin/plugin install license
+bin/plugin install graph
