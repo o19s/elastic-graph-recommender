c86584b9baba0304f13bc35a364373cfc417c6a8
Doug Turnbull <softwaredoug@gmail.com>
2016-10-05 16:48:46 -0400

    Correct to nginx port

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 angular.module('recsApp', ['elasticsearch']);
 
-var esBaseURL = 'http://ec2-54-234-184-186.compute-1.amazonaws.com:9200';
+var esBaseURL = 'http://ec2-54-234-184-186.compute-1.amazonaws.com:9616';
 angular.module('recsApp')
 .service('esClient', function (esFactory) {
   return esFactory({
