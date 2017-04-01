d2b05f2c4f80392e96bbe83de315fec8d5083f19
Eric Pugh <epugh@o19s.com>
2017-03-31 21:19:55 -0400

    fix the ENV variable

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,10 +1,8 @@
 angular.module('recsApp', ['elasticsearch']);
 
 //var esBaseURL = 'http://ec2-54-234-184-186.compute-1.amazonaws.com:9616';
-// We expect a entry called elastic-graph-recommender that points to the
-// Elasticsearch server, typically in your /etc/hosts file.  This is to
-// be Docker friendly.
-var esBaseURL = 'http://localhost:9200';
+// Variable should be updated by srv.sh startup script.
+var esBaseURL = 'http://DYN_ELASTICSEARCH_HOST';
 angular.module('recsApp')
 .service('esClient', function (esFactory) {
   return esFactory({
