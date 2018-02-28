c6a3185adbe24c3ce37f89c489af5d61e680c470
epugh <epugh@opensourceconnections.com>
2018-02-28 14:20:21 -0500

    TMDB changed image poster path sizes, so fix it

--- etl/rehashTmdbToMl.py
+++ etl/rehashTmdbToMl.py
@@ -11,7 +11,7 @@ def rehashToMovielens(tmdbFile='tmdb.json', outFile='ml_tmdb.json'):
             del tmdbMovie['id']
             origPath = tmdbMovie['poster_path']
             if origPath:
-                tmdbMovie['poster_path'] = 'https://image.tmdb.org/t/p/w130' + tmdbMovie['poster_path']
+                tmdbMovie['poster_path'] = 'https://image.tmdb.org/t/p/w154' + tmdbMovie['poster_path']
         except KeyError:
             pass
         mlDict[tmdbMovie['mlensId']] = tmdbMovie
